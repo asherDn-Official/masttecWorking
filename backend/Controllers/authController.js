49db8441e11fce4721cb2c272547635bbaa5acd3
asherDn-Official <developers@asherdn.com>
2025-01-06 11:16:13 +0530

    jwt sign

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -21,23 +21,31 @@ exports.login = async (req, res) => {
 
     // Step 3: Check if employee's department matches
     if (employee.department !== department) {
-      return res.status(403).json({ error: "You are not authorized to access this department" });
+      return res
+        .status(403)
+        .json({ error: "You are not authorized to access this department" });
     }
 
     // Step 4: Check if the employee's role is authorized for the department
     const role = await Role.findOne({ role: employee.designation });
     if (role && role.authorizedPersons.includes(employeeId)) {
       // Step 5: Generate JWT token
-      const token = jwt.sign({ employeeId: employee._id, department: employee.department }, process.env.JWT_SECRET, {
-        expiresIn: "24h", // Token expires in 24 hour
-      });
+      const token = jwt.sign(
+        { employeeId: employee.employeeId, department: employee.department },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h", // Token expires in 24 hour
+        }
+      );
 
       res.status(200).json({
         message: "Login successful",
         token,
       });
     } else {
-      res.status(403).json({ error: "You are not authorized to log in to this department" });
+      res
+        .status(403)
+        .json({ error: "You are not authorized to log in to this department" });
     }
   } catch (error) {
     console.error("Error during login:", error);
